314aab150ab62f9c96cd1d0860ee23a64eeb5281
Nandha Kishore S <nandhakishore.s@prodapt.com>
2024-10-26 14:21:00 +0100

    init

---
+++ src/main/java/com/mine/guidemo/GuidemoApplication.java
@@ -0,0 +1,13 @@
+package com.mine.guidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mine/guidemo/GuidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.guidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
