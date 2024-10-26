5b6f78a76cf08d67bb9828bd65f9fbe49f1906fc
Nandha Kishore S <nandhakishore.s@prodapt.com>
2024-10-26 21:21:17 +0100

    Added GUI, Controller

--- src/main/java/com/mine/guidemo/GuidemoApplication.java
+++ src/main/java/com/mine/guidemo/GuidemoApplication.java
@@ -1,8 +1,6 @@
 package com.mine.guidemo;
 
 import com.mine.guidemo.gui.GUI;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
